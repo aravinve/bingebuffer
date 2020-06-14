bb85e3cde330d775c8b275897d5496b5625dea8f
Aravind Venkatesan <aravindtherocker@gmail.com>
2020-06-14 11:58:57 +0800

    detail commit

--- bingebuffer/booking/urls.py
+++ bingebuffer/booking/urls.py
@@ -5,4 +5,6 @@ app_name = 'booking'
 
 urlpatterns = [
     path('', views.booking_home, name='home'),
+    path('search/', views.booking_search, name='search'),
+    path('detail/<str:id>', views.booking_detail, name='detail'),
 ]
--- bingebuffer/booking/views.py
+++ bingebuffer/booking/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+import requests
 # Create your views here.
 
 @login_required(login_url="/accounts/login")
 def booking_home(request):
     return render(request, 'booking/booking_home.html')
 
+@login_required(login_url="/accounts/login")
+def booking_search(request):
+    searchQuery = request.POST['searchQuery']
+    requestUrl = "http://www.omdbapi.com/?s={}&apikey=497974d4".format(searchQuery)
+    response = requests.get(requestUrl)
+    data = response.json()
+    return render(request, 'booking/booking_home.html', {'movieList': data.get('Search')})
+
+
+@login_required(login_url="/accounts/login")
+def booking_detail(request, id):
+    requestUrl = "http://www.omdbapi.com/?i={}&apikey=497974d4".format(id)
+    response = requests.get(requestUrl)
+    data = response.json()
+    return render(request, 'booking/booking_detail.html', {'movie': data})
 
